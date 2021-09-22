f66f62bb1b2df054519039310f66f8af322d899c
Luis Rangel <80657405+Luis-ran1@users.noreply.github.com>
2021-09-21 22:53:32 -0500

    Proyecto

---
+++ main.js
@@ -0,0 +1,6 @@
+const btnMenu = document.querySelector("#btnMenu");
+const menu = document.querySelector("#menu");
+btnMenu.addEventListener('click', function()
+{
+    menu.classList.toggle("mostrar");
+});
